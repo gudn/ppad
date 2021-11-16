26692f39f04c5db5fc97dba265805dbbf8d05fe6
Daniil Novoselov <gudnmail@gmail.com>
2021-11-16 20:27:07 +0500

    add svelte-navigator

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,13 @@
-<p>ppad</p>
+<script lang="ts">
+  import { Router, Route } from 'svelte-navigator'
+
+  import Home from './pages/Home.svelte'
+  import Document from './pages/Document.svelte'
+</script>
+
+<Router>
+  <Route path="/">
+    <Home />
+  </Route>
+  <Route path="/doc/:id" component={Document} />
+</Router>
