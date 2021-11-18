4f480ba142fb2b3705773d1bb2076552765a34d7
Daniil Novoselov <gudnmail@gmail.com>
2021-11-18 22:26:15 +0500

    components: add input component

---
+++ src/components/Input.svelte
@@ -0,0 +1,36 @@
+<div>
+  <input type="text" />
+  <slot name="icon">
+    <span />
+  </slot>
+</div>
+
+<style lang="scss">
+  @import '../styles/variables.scss';
+
+  div {
+    display: flex;
+    flex-direction: row-reverse;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    background-color: $primary-color;
+    border-radius: 4px;
+
+    input {
+      border-color: transparent;
+      flex: 1;
+      background-color: transparent;
+      color: $text-color;
+      margin-left: -24px;
+      padding: 1px 24px;
+      z-index: 1;
+
+      &:active {
+        outline-color: $text-color;
+      }
+    }
+
+    span {
+      width: 24px;
+    }
+  }
+</style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,19 @@
-<h1>Home page</h1>
+<script lang="ts">
+  import Input from '../components/Input.svelte'
+</script>
+
+<h1>PPad</h1>
+<Input>
+  <span slot="icon" />
+</Input>
+
+<style lang="scss">
+  span {
+    background-image: url('/icons/search.svg');
+    background-size: 20px 20px;
+    min-width: 20px;
+    box-sizing: content-box;
+    margin: 2px;
+    filter: invert(93.3%);
+  }
+</style>
