b52ff8e8f77b8a58fcf243306254d5530106378c
Daniil Novoselov <gudnmail@gmail.com>
2021-11-21 11:40:18 +0500

    Cells: add create button view

--- src/components/Cells.svelte
+++ src/components/Cells.svelte
@@ -5,3 +5,46 @@
 </script>
 
 {cells.toString()}
+
+<footer>
+  <button>
+    <span class="icon" />
+    <span class="label">Create</span>
+  </button>
+</footer>
+
+<style lang="scss">
+  @import '../styles/variables.scss';
+
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    button {
+      align-items: center;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+
+      background-color: $primary-color;
+      color: $text-color;
+      border-radius: 6px;
+      border: none;
+      padding: 0.5em;
+
+      .icon {
+        background-image: url('/icons/add.svg');
+        background-size: 18px 18px;
+        min-width: 18px;
+        min-height: 18px;
+        filter: invert(93.3%);
+        margin-right: 0.3em;
+      }
+
+      .label {
+        margin-top: 2px;
+      }
+    }
+  }
+</style>
