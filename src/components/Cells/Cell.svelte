98d9aceddc1fa1b26f8881ec29299d858a8e1620
Daniil Novoselov <gudnmail@gmail.com>
2021-11-23 16:41:58 +0500

    Cells: add CellButtons wrapper

--- src/components/Cells/Cell.svelte
+++ src/components/Cells/Cell.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
   import type { PCell } from '../../models/cells'
 
+  import CellButtons from './CellButtons.svelte'
+
   export let cell: PCell
 </script>
 
-<div>{cell.key} - {cell.rank}</div>
+<section>
+  <div>
+    {cell.key} - {cell.rank}
+  </div>
+  <CellButtons>
+    Buttons
+  </CellButtons>
+</section>
+
+<style lang="scss">
+  section {
+    position: relative;
+  }
+</style>
---
+++ src/components/Cells/CellButtons.svelte
@@ -0,0 +1,36 @@
+<div class="outer">
+  <div class="inner">
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../../styles/variables.scss';
+
+  .outer {
+    position: absolute;
+    top: 0;
+    right: 1em;
+    min-height: 1.5em;
+    min-width: 10vw;
+
+    &:hover .inner {
+      display: flex !important;
+    }
+  }
+
+  .inner {
+    background-color: $primary-color;
+    border-radius: 4px;
+
+    position: relative;
+    top: -1.6em;
+    padding: 5px;
+
+    height: 1em;
+
+    display: none;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
