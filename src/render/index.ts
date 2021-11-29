bedb52badd4f9fa25f202c8569439d70a411064e
Daniil Novoselov <gudnmail@gmail.com>
2021-11-29 08:02:45 +0500

    fix(render): fix rendering code blocks and latex

--- src/render/index.ts
+++ src/render/index.ts
@@ -1,4 +1,5 @@
 import MarkdownIt from 'markdown-it'
+import { escapeHtml, unescapeAll } from 'markdown-it/lib/common/utils'
 import emoji from 'markdown-it-emoji'
 import sup from 'markdown-it-sup'
 import sub from 'markdown-it-sub'
@@ -27,18 +28,13 @@ const latex = {
 }
 
 const codeRender = {
-  wrap(render: any) {
-    return function (...args: any[]) {
-      return render.apply(this, args)
-    }
-  },
   inline(md: MarkdownIt, tokens: any, idx: number, options: any): string {
     const code = tokens[idx]
     const next = tokens[idx + 1]
     let lang = 'plaintext'
 
     if (next && next.type === 'text') {
-      const match = /^{\.([^}]+)}/.exec(next.content)
+      const match = /^\.([\w-]+)/.exec(next.content)
 
       if (match) {
         lang = match[1]
@@ -47,12 +43,13 @@ const codeRender = {
       }
     }
 
-
     switch (lang) {
       case 'latex':
         return latex.inline(code.content)
       case 'ce':
         return latex.inline(`\\ce{${code.content}}`)
+      case 'latex-source':
+        lang = 'latex'
       default:
         const highlighted = options.highlight(code.content, lang)
         const cls = lang
@@ -62,21 +59,14 @@ const codeRender = {
         return `<code${cls}>${highlighted}</code>`
     }
   },
-  block(code: string, lang: string) {
+  block(code: string, lang: string, _attrs: any) {
     switch (lang) {
       case 'latex':
         return latex.display(code)
-      case 'latex-source':
-        try {
-          return hljs.highlight(code, {
-            language: 'latex',
-            ignoreIllegals: true,
-          }).value
-        } catch (e) {
-          return ''
-        }
       case 'ce':
         return latex.display(`\\ce{${code}}`)
+      case 'latex-source':
+        lang = 'latex'
       default:
         try {
           return lang
@@ -98,14 +88,112 @@ const md = new MarkdownIt({
   typographer: true,
 })
 
+md.renderer.rules.code_block = function (
+  tokens: any[],
+  idx: number,
+  _options: any,
+  _env: any,
+  self: any,
+) {
+  const token = tokens[idx]
+  console.log(tokens)
+  return (
+    '<pre' +
+    self.renderAttrs(token) +
+    '><code>' +
+    escapeHtml(tokens[idx].content) +
+    '</code></pre>\n'
+  )
+}
+md.renderer.rules.code_inline = codeRender.inline.bind(null, md)
+md.renderer.rules.code_block = (
+  tokens: any[],
+  idx: number,
+  _opts: any,
+  _env: any,
+  self: any,
+) => {
+  const token = tokens[idx]
+
+  return (
+    '<pre' +
+    self.renderAttrs(token) +
+    '><code class="hljs">' +
+    escapeHtml(tokens[idx].content) +
+    '</code></pre>\n'
+  )
+}
+md.renderer.rules.fence = (
+  tokens: any[],
+  idx: number,
+  _opts: any,
+  _env: any,
+  self: any,
+) => {
+  const token = tokens[idx],
+    info = token.info ? unescapeAll(token.info).trim() : ''
+  let langName = '',
+    langAttrs = ''
+
+  if (info) {
+    let arr = info.split(/(\s+)/g)
+    langName = arr[0]
+    langAttrs = arr.slice(2).join('')
+  }
+
+  const highlighted =
+    codeRender.block(token.content, langName, langAttrs) ||
+    escapeHtml(token.content)
+
+  if (highlighted.indexOf('<pre') === 0) {
+    return highlighted + '\n'
+  }
+
+  let result = ''
+
+  // If language exists, inject class gently, without modifying original token.
+  // May be, one day we will add .deepClone() for token and simplify this part, but
+  // now we prefer to keep things local.
+  if (info) {
+    switch (langName) {
+      case 'latex':
+      case 'ce':
+        return highlighted
+      default:
+        const i = token.attrIndex('class')
+        const attrs = token.attrs ? token.attrs.slice() : []
+
+        if (i < 0) {
+          attrs.push(['class', 'language-' + langName])
+        } else {
+          attrs[i] = attrs[i].slice()
+          attrs[i][1] += ' ' + 'language-' + langName
+        }
+
+        result =
+          '<pre><code' +
+          self.renderAttrs({ attrs }) +
+          '>' +
+          highlighted +
+          '</code></pre>\n'
+    }
+  }
+
+  result =
+    '<pre><code' +
+    self.renderAttrs(token) +
+    '>' +
+    highlighted +
+    '</code></pre>\n'
+  return result
+    .replace('<code class="', '<code class="hljs ')
+    .replace('<code>', '<code class="hljs">')
+}
+
 md.use(emoji)
 md.use(sup)
 md.use(sub)
 md.use(deflist)
-
-md.renderer.rules.fence = codeRender.wrap(md.renderer.rules.fence)
-md.renderer.rules.code_block = codeRender.wrap(md.renderer.rules.code_block)
-md.renderer.rules.code_inline = codeRender.inline.bind(null, md)
 
 export default function render(s: string): string {
   return md.render(s)
