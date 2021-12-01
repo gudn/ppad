5cb44ff4239ef9450736db6b2b64fbc04633931a
Daniil Novoselov <gudnmail@gmail.com>
2021-12-01 18:21:48 +0500

    wip: sw

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import './styles/root.scss'
 
 init('/am_parse_bg.wasm')
 
+window.addEventListener('load', async () => {
+  if (navigator.serviceWorker) {
+    await navigator.serviceWorker.register('/sw.js')
+  }
+})
+
 const app = new App({
   target: document.body,
 })
